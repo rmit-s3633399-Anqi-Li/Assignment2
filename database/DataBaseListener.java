277fc57a623e1928da4034ece6f5394b6518cc19
rmit-s3633399-Anqi-Li <31309552+rmit-s3633399-Anqi-Li@users.noreply.github.com>
2018-05-19 23:05:37 +1000

    Add files via upload

---
+++ database/DataBaseListener.java
@@ -0,0 +1,11 @@
+package com.socialnetwork.assign2.database;
+/**
+ * @author Anqi Li s3633399 
+ */
+public interface DataBaseListener {
+	
+	public void onPersonListChanged();
+	
+	public void onRelationshipChanged();
+
+}
