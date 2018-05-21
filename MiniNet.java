f789f0157c81ac573506b071f853061e6e6738c3
rmit-s3633399-Anqi-Li <31309552+rmit-s3633399-Anqi-Li@users.noreply.github.com>
2018-05-22 00:06:39 +1000

    Add files via upload

---
+++ MiniNet.java
@@ -0,0 +1,43 @@
+package com.socialnetwork.assign2;
+/**
+ * @author Anqi Li s3633399
+ */
+import java.util.ArrayList;
+
+import com.socialnetwork.assign2.dao.Relation;
+import com.socialnetwork.assign2.ui.BasicInfoFrame;
+
+/**
+ * 
+ * main startup class
+ *
+ */
+public class MiniNet {
+
+	public MiniNet() {
+
+	}
+
+	public static void main(String[] args) {
+		
+		//test
+		System.out.println("test relation arraylist");
+		ArrayList<Relation> relations = new ArrayList<Relation>();
+		relations.add(new Relation("aa","bb","cc"));
+		Relation r1 = new Relation("aa","bb","cc");
+		if(!relations.contains(r1)){
+			relations.add(r1);
+		}
+		r1 = new Relation("bb","aa","cc");
+		if(!relations.contains(r1)){
+			relations.add(r1);
+		}
+		for(Relation r : relations){
+			System.out.println(r);
+		}
+		
+		new BasicInfoFrame();
+
+	}
+
+}
