21cb181500cf89a8ecd23f9a0ba058d0f385c25b
Yinanjin <37734938+Yinanjin@users.noreply.github.com>
2018-05-22 01:20:41 +1000

    Add files via upload

---
+++ exception/DifferentIdentitiesException.java
@@ -0,0 +1,5 @@
+package com.socialnetwork.assign2.exceptions;
+
+public class DifferentIdentitiesException extends SocialNetworkException {
+
+}
---
+++ exception/HasParentsException.java
@@ -0,0 +1,5 @@
+package com.socialnetwork.assign2.exceptions;
+
+public class HasParentsException extends SocialNetworkException {
+
+}
