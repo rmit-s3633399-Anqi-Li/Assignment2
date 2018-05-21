911a478a996235d27e4590deafc916a50b69c041
Yinanjin <37734938+Yinanjin@users.noreply.github.com>
2018-05-22 01:18:01 +1000

    Add files via upload

---
+++ gui/AllAdultsOrAllChildrenException.java
@@ -0,0 +1,5 @@
+package com.socialnetwork.assign2.exceptions;
+
+public class AllAdultsOrAllChildrenException extends SocialNetworkException {
+
+}
---
+++ gui/AlreadyColleaguesException.java
@@ -0,0 +1,5 @@
+package com.socialnetwork.assign2.exceptions;
+
+public class AlreadyColleaguesException extends SocialNetworkException {
+
+}
