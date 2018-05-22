d460c8aab30b17d667e80d90c6b533edb2473079
Yinanjin <37734938+Yinanjin@users.noreply.github.com>
2018-05-22 14:13:57 +1000

    Add files via upload

--- Child.java
+++ Child.java
@@ -3,18 +3,16 @@ package com.socialnetwork.assign2.dao;
 import java.util.ArrayList;
 
 /**
- * @author Anqi Li s3633399
- * subclass Child inherits from superclass Person
- * Child(dependent)
+ * Teenager(dependent)
  * Description:
  * 
- * Other than .friendships, there are other types of connections between people. 
+ * Other than “friendships”, there are other types of connections between people. 
  * Some persons on the network are under 16 years of age. They are considered as dependents to adults. 
  * No isolated dependent can exist on the network meaning a dependent must have some connections. 
  * They always connect to exactly two adults who are the parents. The parents of a dependent must connect. 
  * You can assume there are no orphans, no single parents in this scenario.
  *  All couples are mutually exclusive to other couples. Situations like “A and B are the parents of X, 
- *  A and C are the parent of C do not exist in this exercise. However an adult may have no connections.
+ *  A and C are the parent of Y” do not exist in this exercise. However an adult may have no connections.
  *  A dependent can be a friend ONLY to another dependent who is also younger than 16 and from a different family. 
  *  The age difference between these two young friends cannot be more than 3 years. 
  *  A person who is 2 years old or younger does not have any friends. 
